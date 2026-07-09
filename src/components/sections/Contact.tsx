13afb987f11782d01db2840c706fd873def9156c
Nicolas Sarmiento <nicolas.sarmiento.jimenez@gmail.com>
2026-07-09 12:51:01 -0400

    feat: update CV file

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -118,7 +118,7 @@ const Contact = () => {
                   type="primary"
                   size="large"
                   className="cta-button"
-                  href="/cv/CV_Nicolas_Sarmiento_2026_Final.pdf"
+                  href="/cv/CV_Nicolas_Sarmiento_2026.pdf"
                   download
                 >
                   Descargar CV
